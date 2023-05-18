e19784991d80e19b91dad0b6b676c75ca3ec9f49
DeathlyBower959 <DeathlyBower959yt@gmail.com>
2023-05-18 19:13:56 -0400

    fix redirect issue?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 export default function Home() {
-  redirect('https://jiayi.software');
+  return (
+    <div style={{ display: 'flex', gap: '2em' }}>
+      <a href='https://jiayi.software'>Jiayi Software</a>
+      <Link href='/docs'>Docs</Link>
+    </div>
+  );
 }

90795d30967f45ad77da2c0d4138194efff0abc7
DeathlyBower959 <DeathlyBower959yt@gmail.com>
2023-06-04 20:55:33 -0400

    phucking tailwind defaults man

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import globeASCII from './assets/ascii/globe';
 export default function Home() {
   return (
     <div className='w-min'>
-      <pre>{globeASCII}</pre>
+      <pre className='my-4'>{globeASCII}</pre>
       <p className='text-center w-full'>
-        <a href='https://jiayi.software' className='text-pure'>
+        <a href='https://jiayi.software' className='text-pure underline'>
           jiayi software
         </a>{' '}
         &#x2022;{' '}
-        <Link href='/docs' className='text-pure'>
+        <Link href='/docs' className='text-pure underline'>
           api docs
         </Link>
       </p>
